5405b7a9176b29b1b576136cdcc2b956fad76da4
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2020-11-29 15:19:13 +0200

    Design Patterns Exercises

---
+++ Design Patterns - Lab/Singleton/SingletonDataContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Singleton
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+
+        private Dictionary<string, int> capitals = new Dictionary<string, int>();
+
+        public SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < elements.Length; i++)
+            {
+                capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+        public static SingletonDataContainer Instance => instance;
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+}
