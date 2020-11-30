d61da5449ed6d8e081b59e490b1f823fad8e7364
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2020-11-30 18:42:22 +0200

    Design Patterns Exercise

---
+++ Design Patterns - Lab/Facade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new CarBuilderFacade()
+              .Info
+                .WithType("BMW")
+                .WithColor("Blue")
+                .WithNumberOfDoors(3)
+              .Built
+                .InCity("Munich")
+                .AtAddress("BMW HeadQuaters")
+              .Build();
+
+            Console.WriteLine(car);
+        }
+    }
+}
