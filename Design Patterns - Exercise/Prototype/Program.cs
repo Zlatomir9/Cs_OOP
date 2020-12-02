1de7a838fb4c32a5f6f3b701f5a3ff2b844d5149
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2020-12-02 19:14:02 +0200

    Design Patterns Exercise

---
+++ Design Patterns - Exercise/Prototype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink drink = new Drink("Coke");
+            Sandwich sandwich = new Sandwich("bread", "cheese", "veggies", "meat", drink);
+            Sandwich sandwich2 = sandwich.ShallowCopy();
+            Sandwich sandwich3 = sandwich.DeepCopy();
+
+            Console.WriteLine("Original: " + sandwich);
+            Console.WriteLine("Shallow copy: " + sandwich2);
+            Console.WriteLine("Deep copy: " + sandwich3);
+            Console.WriteLine();
+
+            sandwich.Bread = "dark bread";
+            sandwich.Cheese = "blue cheese";
+            sandwich.Veggies = "tomato";
+            sandwich.Meat = "chicken";
+            sandwich.Drink.Name = "cold tea";
+
+            Console.WriteLine("Original: " + sandwich);
+            Console.WriteLine("Shallow copy: " + sandwich2);
+            Console.WriteLine("Deep copy: " + sandwich3);
+        }
+    }
+}
